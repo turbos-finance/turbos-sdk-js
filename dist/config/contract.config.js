d7eb645f766ba9863619156bf0ef974dbe1dbbe4
blockJser@gmx.com <blockJser@gmx.com>
2023-02-08 14:52:30 +0800

    feat:build

--- dist/config/contract.config.js
+++ dist/config/contract.config.js
@@ -7,12 +7,12 @@ exports.contractConfig = {
         "VaultObjectId": "0x63847d33c6256f0fd8cbd86d855a59e69be8f093",
         "PositionsObjectId": "0xc6a061a161c9e2f43a4c5bcefdf173a96ffc4f82",
         "ManagerCapObjectId": "0x1e2e578f2b2dfef2b579bccd0b0acc21229865bb",
-        "TimeOraclePackageId": "0x6062468b231618209bde33ecfa5f1014301ac561",
-        "PriceOraclePackageId": "0x84edceb39b6a683ee64078e7249f7b4e2df2c093",
-        "PriceOracleAuthorityCapObjectId": "0xf7709911a64688bc13baec7f7f7464022967c793",
-        "TimeOracleObjectId": "0x39b745e686d6c77f9b10dbe04eb57cd43b42d4d9",
-        "TimeOracleAuthorityCapObjectId": "0x87d3cc3c9fc661c1532907328f35ff56774873bc",
-        "PriceFeedStorageObjectId": "0x43c12303f3a6d34ccff59118142dba0c048b00cb"
+        "TimeOraclePackageId": "0x87ee14eb7526a1a4cd87e394b31d64d5b9fc7f0a",
+        "PriceOraclePackageId": "0xce763e6fa2be4e319ba90676a9b0af3be78c1973",
+        "PriceOracleAuthorityCapObjectId": "0xe52698cab3b137b7b02951b28ce8c7c0e94553a7",
+        "TimeOracleObjectId": "0x97b8bb469e316be74efec80bb0f64ed67e8e2a79",
+        "TimeOracleAuthorityCapObjectId": "0x62109ee80c74b26a04c14415feec138ddb5d77b1",
+        "PriceFeedStorageObjectId": "0x1c4ad3ce189604de528a1a6ee5a4adc3840ea3cd"
     },
     "TESTNET": {
         "ExchangePackageId": "0x88b3387f36c92e0fa79019c3997e43dcb248a158",

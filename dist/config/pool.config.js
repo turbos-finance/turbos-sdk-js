a4b43c8a50fa6d92c4a9b09b3b0a88f1395c0efd
lyiyun1024 <lyiyun1024@gmail.com>
2023-03-29 15:21:48 +0800

    feat: devnet 0.29.0

--- dist/config/pool.config.js
+++ dist/config/pool.config.js
@@ -4,36 +4,36 @@ exports.symbolPoolsConfig = void 0;
 exports.symbolPoolsConfig = {
     "DEVNET": {
         "BTC": {
-            "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+            "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
             "PriceFeedObjectId": "0x1c685138bc08bc8b38a65e5b071e21bbd2d2f1df",
             "PriceFeedId": "fbd7c495fcc83ec7ce6522eb44a453a70f88ef64664f1ed49e011be87ffe3525",
-            "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::btc::BTC",
-            "PoolObjectId": "0x84ed6dc9d1c39086255d5743fea17ea9d6613a02",
-            "PoolDataObjectId": "0xf679545a485b673d9a430562455d421ffb3eb199"
+            "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::btc::BTC",
+            "PoolObjectId": "0x69462886e525678bf065c8afa6f24801d47a4dee",
+            "PoolDataObjectId": "0xe8664bc6430e8eb1012ca7ca7ffaf56359a9bb1d"
         },
         "ETH": {
-            "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+            "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
             "PriceFeedObjectId": "0xcfa94cc65a45fb0668e69585e984d918b56951ce",
             "PriceFeedId": "3b3852469b9667b95ce1ef1fe4ceba4ebba4e07b42acda4e8e3246598e5cb73f",
-            "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::eth::ETH",
-            "PoolObjectId": "0xddaea92e4a669d16993abf184c54e30570e16367",
-            "PoolDataObjectId": "0xffe471b8fac9aa2725894cafd37f16d266d88471"
+            "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::eth::ETH",
+            "PoolObjectId": "0x6d671bd710ffc1d6c3ee7e3321f73160223ba42e",
+            "PoolDataObjectId": "0x4ea8f453b6a7ef412d26d54a705688ed599d6403"
         },
         "USDC": {
-            "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+            "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
             "PriceFeedObjectId": "0x12f0340cefa25f9bf3cee8e39277f7181b96e072",
             "PriceFeedId": "348eb3a88317813f7541e4008843b907822259c008a2793e43edce2d1360e27d",
-            "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::usdc::USDC",
-            "PoolObjectId": "0xeb39d221a4d8dd65059d097dcface15aae8171d4",
-            "PoolDataObjectId": "0xab9976b6e913f5c9ae3a4f9b6a38e5db2d3f562e"
+            "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::usdc::USDC",
+            "PoolObjectId": "0x2df23e6ec6e5f109788c04be09849ebe2a8848ed",
+            "PoolDataObjectId": "0xf6d9183b7e1b717065e089df40b37a9ea908b5a2"
         },
         "SUI": {
             "PackageId": "0x0000000000000000000000000000000000000002",
             "PriceFeedObjectId": "0x33935818758ed932364398691da48e3b9bd84b66",
             "PriceFeedId": "16c3533d98cb020a2304c7f26169ac2597079f9aac853dd141d0aff86f540d56",
             "Type": "0x0000000000000000000000000000000000000002::sui::SUI",
-            "PoolObjectId": "0xee77a022fb93608856239846a473470fd854cce4",
-            "PoolDataObjectId": "0xa1ff14c1e75eed376716ede65fa58e4c134c6d55"
+            "PoolObjectId": "0x48ed42932db5c1e9197980867297a6e554dc1b54",
+            "PoolDataObjectId": "0x79475ebe6f26fdd91ccd984574c8e1a7b404d433"
         }
     },
     "TESTNET": {
--- lib/config/pool.config.ts
+++ lib/config/pool.config.ts
@@ -3,26 +3,26 @@ import { SymbolPoolsConfig } from "../types/config.type";
 export const symbolPoolsConfig: SymbolPoolsConfig = {
   "DEVNET": {
     "BTC": {
-      "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+      "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
       "PriceFeedObjectId": "0x1c685138bc08bc8b38a65e5b071e21bbd2d2f1df",
       "PriceFeedId": "fbd7c495fcc83ec7ce6522eb44a453a70f88ef64664f1ed49e011be87ffe3525",
-      "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::btc::BTC",
+      "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::btc::BTC",
       "PoolObjectId": "0x69462886e525678bf065c8afa6f24801d47a4dee",
       "PoolDataObjectId": "0xe8664bc6430e8eb1012ca7ca7ffaf56359a9bb1d"
     },
     "ETH": {
-      "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+      "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
       "PriceFeedObjectId": "0xcfa94cc65a45fb0668e69585e984d918b56951ce",
       "PriceFeedId": "3b3852469b9667b95ce1ef1fe4ceba4ebba4e07b42acda4e8e3246598e5cb73f",
-      "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::eth::ETH",
+      "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::eth::ETH",
       "PoolObjectId": "0x6d671bd710ffc1d6c3ee7e3321f73160223ba42e",
       "PoolDataObjectId": "0x4ea8f453b6a7ef412d26d54a705688ed599d6403"
     },
     "USDC": {
-      "PackageId": "0x47084a24e5a3024107586105f9feda075e0b39c4",
+      "PackageId": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c",
       "PriceFeedObjectId": "0x12f0340cefa25f9bf3cee8e39277f7181b96e072",
       "PriceFeedId": "348eb3a88317813f7541e4008843b907822259c008a2793e43edce2d1360e27d",
-      "Type": "0x47084a24e5a3024107586105f9feda075e0b39c4::usdc::USDC",
+      "Type": "0xfaf4e2441c904cd67d24704ff635587a7c9156c5f0d6f0b75d67b8c2f9f40c8c::usdc::USDC",
       "PoolObjectId": "0x2df23e6ec6e5f109788c04be09849ebe2a8848ed",
       "PoolDataObjectId": "0xf6d9183b7e1b717065e089df40b37a9ea908b5a2"
     },
